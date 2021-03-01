516bd8b44dcbd40fa6f38aa621e953f1a0bd8eff
Kony <fzf55747@kwansei.ac.jp>
2021-03-01 15:59:02 +0900

    ViewControllerのSwiftファイルを作成し紐付け

---
+++ WhoIsEpisodeKing/PunishmentGameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PunishmentGameViewController.swift
+//  WhoIsEpisodeKing
+//
+//  Created by 小西星七 on 2021/03/01.
+//
+
+import UIKit
+
+class PunishmentGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
